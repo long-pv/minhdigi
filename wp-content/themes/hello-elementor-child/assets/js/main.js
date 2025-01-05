7916070e5a95c13eed17089c6796b62d0f7170ef
longpv <long05062000@gmail.com>
2025-01-06 02:18:09 +0700

    coding form đăng ký

--- wp-content/themes/hello-elementor-child/assets/js/main.js
+++ wp-content/themes/hello-elementor-child/assets/js/main.js
@@ -2,8 +2,14 @@
 	// longpv ---------------------
 	//
 	$(".tab_custom_1 .e-n-tabs-heading").on("wheel", function (event) {
+		// Ngăn chặn cuộn dọc mặc định
 		event.preventDefault();
+		// Cuộn ngang dựa trên cuộn dọc
 		this.scrollLeft += event.originalEvent.deltaY;
+	});
+
+	$(".btn_dang_ky").on("click", function () {
+		$(".form_dang_ky .wpcf7-submit").trigger("click");
 	});
 	// vucoder --------------------
 	//
