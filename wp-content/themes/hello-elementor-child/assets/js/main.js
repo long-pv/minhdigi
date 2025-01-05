bb0b26491161ed0e706a11ab65dcea4ea66bc2d7
longpv <longpv@codluck.com>
2025-01-05 11:49:34 +0700

    plugin contact form 7

--- wp-content/themes/hello-elementor-child/assets/js/main.js
+++ wp-content/themes/hello-elementor-child/assets/js/main.js
@@ -1,6 +1,10 @@
 (function ($, window) {
 	// longpv ---------------------
 	//
+	$(".tab_custom_1 .e-n-tabs-heading").on("wheel", function (event) {
+		event.preventDefault();
+		this.scrollLeft += event.originalEvent.deltaY;
+	});
 	// vucoder --------------------
 	//
 })(jQuery, window);
